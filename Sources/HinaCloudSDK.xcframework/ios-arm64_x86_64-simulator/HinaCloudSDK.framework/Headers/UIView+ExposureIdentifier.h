1bffc808bc250712affb6c11701e665461e38533
邓昊 <denghao@hinadt.com>
2023-11-10 10:58:55 +0800

    更新 version = 3.8.1 tag = 0.3.3 config = xcframework

---
+++ Sources/HinaCloudSDK.xcframework/ios-arm64_x86_64-simulator/HinaCloudSDK.framework/Headers/UIView+ExposureIdentifier.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (HNExposureIdentifier)
+
+@property (nonatomic, copy, nullable) NSString *exposureIdentifier;
+
+@end
+
+NS_ASSUME_NONNULL_END
